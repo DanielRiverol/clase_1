95eab6a74260a4b5983d58e99d55f17fba2cd583
Daniel Riverol <dannyriverol@hotmail.com>
2023-02-27 21:25:46 -0300

    Clase_1 Variables

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//Variables ES5 var nombre
+//declaracion
+var descriptionDeVariable = "Soy un a variable";
+var descriptionDeVariable = "Soy una nueva descripcion";
+
+//ES6
+let nombre = "Daniel";
+
+//dando otro valor
+nombre = "Juan";
+
+let apellido;
+//asignando
+apellido = "Gomez";
+
+//reasignando
+apellido = "Solano";
+let nombreCompleto;
+
+nombreCompleto = "Daniel Gomez";
+const PI = 3.14159;
+const IVA = 21;
+
+//otros vslore
+let usuario = "pepe";
+let pass = 1234;
+
+pass = "0928";
+
+let numero1 = 123;
+let numero2 = 345;
+
+let resultado = numero1 + numero2;
